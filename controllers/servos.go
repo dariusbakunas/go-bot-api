5ff431de3fe488b2a861f6e6316385c3432c7169
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-10-13 20:27:12 -0400

    initial turn implementation

--- controllers/servos.go
+++ controllers/servos.go
@@ -13,15 +13,32 @@ type ServosController struct{
 }
 
 func (s ServosController) Turn(c *gin.Context) {
-	id := c.Param("id")
-
 	angle, err := strconv.Atoi(c.Query("angle"))
 
 	if err != nil || angle < 0 || angle > 180 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid angle"})
+		c.Abort()
+		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"status": fmt.Sprintf("%s: %s", id, angle)})
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil || id < 0 || id > 31 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Servo ID must be between 0 and 31"})
+	}
+
+	// TODO: make this configurable
+	pulseRange := 2100.0 - 900
+	pulse :=  int(900 + pulseRange/180.0 * float64(angle))
+
+	command := fmt.Sprintf("#%d P%d \r", id, pulse)
+	_, err = s.ssc32u.Write(command)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
 }
 
 func ServosControllerInit(ssc32u *models.SSC32U) *ServosController {
